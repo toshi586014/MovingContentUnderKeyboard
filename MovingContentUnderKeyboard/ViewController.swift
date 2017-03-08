25d0f79adb326b6bb88ca16bb757749a466763d2
Toshiaki Nakamura <toshi586014@icloud.com>
2017-03-08 10:14:15 +0900

    Initial Commit

---
+++ MovingContentUnderKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovingContentUnderKeyboard
+//
+//  Created by Toshiaki Nakamura on 2017/03/08.
+//  Copyright © 2017年 Toshiaki Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
